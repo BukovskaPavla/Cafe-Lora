003d23f1949efb0c30743c11392bd8244ed17690
BukovskaPavla <bukovskapavla@gmail.com>
2022-04-30 10:11:54 +0200

    hamburger menu odkazuje a zavira se po kliknuti

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navBtn = document.querySelector("#nav-btn")
+const navNav = document.querySelectorAll("nav a")
+const nav = document.querySelector("nav")
+
+navBtn.addEventListener("click", () => {
+    nav.classList.toggle("nav-closed")
+})
+
+
+/* navNav.addEventListener("click", () => {
+    console.log("jsem v menu")
+    nav.classList.add("nav-closed")
+}) */
+
+navNav.forEach((elm) => {
+    elm.addEventListener('click', () => {
+        nav.classList.add("nav-closed")
+    });
+  });
